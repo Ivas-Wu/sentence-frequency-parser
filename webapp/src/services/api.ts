fdbf29d2406c640c75f1943d56b16905c60a0a7d
Ivas Wu <ivas.wu@yahoo.ca>
2025-01-21 23:06:50 -0500

    get client ready for server

--- webapp/src/services/api.ts
+++ webapp/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { DuplicateMapping } from '../types/api';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 export const getDuplicates = async (
     text: string,
